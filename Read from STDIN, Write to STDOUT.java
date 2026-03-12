56804564f583a6f3d9309a2d68c0a82a20a0a7ed
Priyadharshini R <priyapriyabalamurugan@gmail.com>
2026-03-12 18:56:36 +0530

    Implement basic input and output functionality(hackerearth)

---
+++ Read from STDIN, Write to STDOUT.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String s= sc.next();
+        System.out.println(n * 2);
+        System.out.println(s);
+    }
+}
+Check the link https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/io-exercise/
